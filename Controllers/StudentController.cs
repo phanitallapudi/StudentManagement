37f0d55758fc8ceebdd66a749a88d7e56331cb76
Panindhra Tallapudi <phanitallapudi@gmail.com>
2023-03-29 09:42:56 +0530

    Basic Login, add, delete, edit application

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        DbServicesContext context = new DbServicesContext();
+
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(context.Std_Table.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student stud)
+        {
+            context.Std_Table.Add(stud);
+            int count = context.SaveChanges();
+
+            if (count > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.SubmitMsg = ("<script>alert('Error occured')</script>");
+
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var elem = context.Std_Table.Where(model => model.Id == id).FirstOrDefault();
+
+            return View(elem);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student stud)
+        {
+            context.Entry(stud).State = EntityState.Modified;
+            int change = context.SaveChanges();
+
+            if (change > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.EditMdsg = ("<script>alert('Error occured')</script>");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var changes = context.Std_Table.Where(model => model.Id == id).FirstOrDefault();
+            return View(changes);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Student stud)
+        {
+            context.Entry(stud).State = EntityState.Deleted;
+            int change = context.SaveChanges();
+
+            if (change > 0)
+            {
+                ModelState.Clear();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.EditMdsg = ("<script>alert('Error occured')</script>");
+            }
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Student stud)
+        {
+            var row = context.Std_Table.Where(model => model.Email == stud.Email && model.Password == stud.Password).FirstOrDefault();
+            //int count = context.SaveChanges();
+
+            if (row != null)
+            {
+                return RedirectToAction("Welcome");
+            }
+            else
+            {
+                ViewBag.LoginMsg = ("<script>alert('Incorrect credentials')</script>");
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            //var row = context.Std_Table.Where
+
+            return View(context.Std_Table.ToList());
+        }
+
+
+    }
+}
---
+++ Models/DbServicesContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.Models
+{
+    public class DbServicesContext : DbContext
+    {
+        public DbSet<Student> Std_Table { get; set; }
+    }
+}
